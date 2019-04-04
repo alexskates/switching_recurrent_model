936a91348188ce8a09f123c0e6babd51a52b6296
Alexander Skates <alexander.skates@ohba.ox.ac.uk>
2019-04-04 01:12:34 +0100

    No longer calculating loss for entire sequence

--- src/models.py
+++ src/models.py
@@ -506,7 +506,7 @@ def heirarchical_nll(x, expected_values, covariances):
 
 
 def gumbel_vfe(x, expected_values, covariances, variational_distribution,
-               prior_distribution, original_data_length):
+               prior_distribution):
     # KL divergence
     q_z = variational_distribution['z'].contiguous()
     p_z = prior_distribution['z'].contiguous()
@@ -534,7 +534,7 @@ def gumbel_nll(x, expected_values, covariances):
 
 
 def gumbel_normal_vfe(x, expected_values, covariances, variational_distribution,
-                      prior_distribution, original_data_length):
+                      prior_distribution):
     # Negative log likelihood
     ll = loglik_mixture(x.contiguous(),
                         expected_values['z'].contiguous(),
